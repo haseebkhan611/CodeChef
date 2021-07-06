6d166204245b356ee072665b12943b63bfeacdd5
haseebkhan611 <haseebkhan611@users.noreply.github.com>
2021-07-06 17:31:29 +0500

    Update maximumProduction.py

--- July2021/maximumProduction.py
+++ July2021/maximumProduction.py
@@ -1,4 +1,11 @@
 
+"""
+Chefland has 7 days in a week. Chef is very conscious about his work done during the week.
+
+There are two ways he can spend his energy during the week. The first way is to do x units of work every day and the second way is to do y (>x) units of work for the first d (<7) days and to do z (<x) units of work thereafter since he will get tired of working more in the initial few days.
+
+Find the maximum amount of work he can do during the week if he is free to choose either of the two strategies."""
+
 
 cases=int(input())
 
@@ -7,4 +14,3 @@ for i in range(cases):
     val1=(7*x)
     val2=(y*d)+(z*(7-d))
     print(max(val1,val2))
-print('abc')
